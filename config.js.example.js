11e14f348692edca3afaadb5a4793221e6135fbf
unknown <agordillos@gmail.com>
2025-11-07 18:57:18 +0100

    Config example file

---
+++ config.js.example.js
@@ -0,0 +1,19 @@
+//Copy this file to config.js and specify your own settings
+
+export let ESCAPP_APP_SETTINGS = {
+  //Settings that can be specified by the authors
+  //background: "NONE", //background can be "NONE" or a URL.
+  actionAfterSolve: "SHOW_MESSAGE", //actionAfterSolve can be "NONE" or "SHOW_MESSAGE".
+  //message: "Custom message",
+
+  //Settings that will be automatically specified by the Escapp server
+  solutionLength: 4,
+  locale:"es",
+
+  escappClientSettings: {
+    endpoint:"https://escapp.es/api/escapeRooms/id",
+    linkedPuzzleIds: [1],
+    rtc: false,
+    preview: false
+  },
+};
